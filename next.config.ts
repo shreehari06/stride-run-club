e258d55b56beeb705f43111c57f9fa3bfe5c5f5c
Shreehari Thakral <shreehari_thakral@mckinsey.com>
2025-03-17 00:12:40 +0530

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,14 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  // basePath: isProd ? "/stride-run-club" : "",
-  // assetPrefix: isProd ? "/stride-run-club" : "",
+  basePath: isProd ? "/stride-run-club" : "",
+  assetPrefix: isProd ? "/stride-run-club" : "",
   output: "export",
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
   images: {
     unoptimized: true,
   },
