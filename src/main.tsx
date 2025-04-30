f000aa5aaa9a26f143f7ef4811cf7ba502ee6a5c
Shreehari Thakral <shreehari_thakral@mckinsey.com>
2025-04-30 14:56:46 +0530

    Remove Next.js specific files and fix imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./app/globals.css";
+import "./globals.css";
 import "./fontStyles.css";
 
 console.log("Main.tsx is executing");
